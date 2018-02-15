b2d7d8b67d9fe65187572e62122c53329767cfc6
Robert Schrom <robertschrom2@gmail.com>
2018-02-14 21:23:34 -0500

    Update ftest_nompi.f90

--- ftest_nompi.f90
+++ ftest_nompi.f90
@@ -12,16 +12,10 @@ program ftest
     end do
 
     ! calculate dummy values
-
-    ! open mpi commands
-    !$omp parallel private(i) shared(avals,dum)
-    !$omp do
     do i = 1, nval
         dum(i) = dummy(avals(i))
         print *, dum(i)
     end do
-    !$omp end do
-    !$omp end parallel
 
     print *, dum
 
