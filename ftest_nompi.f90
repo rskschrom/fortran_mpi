8413a0ff11c656c27fa8fc2c5ab253ef7f55ffd7
Robert Schrom <snowwxradar@users.noreply.github.com>
2018-02-14 21:22:49 -0500

    added no-mpi version of code

---
+++ ftest_nompi.f90
@@ -0,0 +1,28 @@
+program ftest
+    use test_funcs
+    implicit none
+
+    integer :: i, comm, rank, numproc, ierr
+    integer, parameter :: nval = 40
+    real, dimension(nval) :: dum, avals
+
+    ! loop through and set avals
+    do i = 1, nval
+        avals(i) = real(i)/real(nval)
+    end do
+
+    ! calculate dummy values
+
+    ! open mpi commands
+    !$omp parallel private(i) shared(avals,dum)
+    !$omp do
+    do i = 1, nval
+        dum(i) = dummy(avals(i))
+        print *, dum(i)
+    end do
+    !$omp end do
+    !$omp end parallel
+
+    print *, dum
+
+end program
